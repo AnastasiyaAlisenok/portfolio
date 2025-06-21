ff5e5543dc1600b552a82977942bba2afb49fed5
Anastasiya <119356164+AnastasiyaAlisenok@users.noreply.github.com>
2025-06-21 17:28:05 +0300

    Update Contacts.tsx

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -7,7 +7,7 @@ import { Contact } from "./Contact/Contact";
 
 const contactsPath = {
   git: "https://github.com/AnastasiyaAlisenok",
-  telegram: "https://t.me/",
+  telegram: "",
   linkedin:
     "https://www.linkedin.com/in/%D0%B0%D0%BD%D0%B0%D1%81%D1%82%D0%B0%D1%81%D0%B8%D1%8F-%D0%B0%D0%BB%D0%B8%D1%81%D0%B5%D0%BD%D0%BE%D0%BA-323108293/",
 };
